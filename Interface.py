bbaf301f9fd876c9f9e55d18bf95453fad4c669b
Mohammed <mobj@cmu.edu>
2020-04-26 06:27:27 +0300

    Major bug fixes + polyomino placement generation
    commit

--- Interface.py
+++ Interface.py
@@ -1,83 +1,40 @@
 #################################################################
-################### LOOKING AT SHAPES PROJECT ###################
-######## Authors: Mohammed Al-jawaheri && Igli Mlloja ###########
+################### LOOKING AT SHAPES PROJECT ###################
+######## Authors: Mohammed Al-jawaheri && Igli Mlloja ###########
 #################################################################
-# Time: 2:01 AM
-
+# Time: 2:01 AM
 #
-## Polyomino Tiler, implemented intrepreting grids as bipartite graphs
-# and attempting to solve exact cover. We consider a potential polyomino
-# placement a node in a bipartite graph with edges connected to all of
-# the cells it occupies in a given placement.
-#     A      B    where A is set of a potential placement of all n polyominos
-#     * ---> *    and B is set of all cells in the grid
-#     * ---> *
-#     * ---> *
-#     * ---> *
-#     * ---> *
-
-
-
+## Polyomino Tiler, implemented intrepreting grids as bipartite graphs
+# and attempting to solve exact cover. We consider a potential polyomino
+# placement a node in a bipartite graph with edges connected to all of
+# the cells it occupies in a given placement.
+#     A      B    where A is set of a potential placement of all n polyominos
+#     * ---> *    and B is set of all cells in the grid
+#     * ---> *
+#     * ---> *
+#     * ---> *
+#     * ---> *
 import math, copy, random
 import graph_backend
 from cmu_112_graphics import *
 
 #################################################
-# Configuration function
+# Configuration function
 #################################################
 def gridDimensions():
-    # return (15,10,20,25)
     return (30, 25, 20, 25)
 
-# BASIC CONSTS
-# Seven "standard" pieces (tetrominoes)
-# Initialize the pieces
-iPiece = [
-    [  True,  True,  True,  True ]
-]
-
-jPiece = [
-    [  True, False, False ],
-    [  True,  True,  True ]
-]
-
-lPiece = [
-    [ False, False,  True ],
-    [  True,  True,  True ]
-]
-
-oPiece = [
-    [  True,  True ],
-    [  True,  True ]
-]
-
-sPiece = [
-    [ False,  True,  True ],
-    [  True,  True, False ]
-]
-
-tPiece = [
-    [ False,  True, False ],
-    [  True,  True,  True ]
-]
-
-zPiece = [
-    [  True,  True, False ],
-    [ False,  True,  True ]
-]
-
-
-# Helper functions for determining disconnected subcomponents of a grid
+# Helper functions for determining disconnected subcomponents of a grid
 #
-## Get size of a connected component of grid
+## Get size of a connected component of grid
 def floodFill(grid, i, j):
     board = grid.board
-    # look for a pl ace to start filling
+    # look for a place to start filling the board
     if board[i][j] == grid.emptyColor:   # access is safe; edges are occupied
-        return 1 + floodFill(grid, i + 1, j) + floodFill(grid, i - 1, j) + floodFill(grid, i, j + 1) + floodFill(grid, i, j - 1)
+        return 1 + floodFill(grid, i + 1, j) + floodFill(grid, i - 1, j)\
+                + floodFill(grid, i, j + 1) + floodFill(grid, i, j - 1)
     else:
         return 0
-
 
 
 # Our grid for making the polyomino representation
@@ -90,8 +47,7 @@ class grid:
         self.cellSize = self.myDimensions[2]
         self.margin = self.myDimensions[3]
         self.whiteBlocks = (self.rows-2)*(self.cols-2)
-
-
+        self.connectedComponentCount = []
         # initialize the board
         self.emptyColor = "white"
         self.board = [[self.emptyColor for x in range(self.cols)] for x in range(self.rows)]
@@ -102,30 +58,41 @@ class grid:
             self.board[i][0] = "black"
             self.board[i][self.cols-1] = "black"
 
+
     def blockifyCell(self, row, col):
-        if row != 0 and row != self.rows-1 and col != 0 and col != self.cols -1:
+        # check for out of bounds
+        if row > 0 and row < self.rows-1 and col > 0 and col < self.cols-1:
+            # switch white to black
             if self.board[row][col] == self.emptyColor:
                 self.board[row][col] = "black"
                 self.whiteBlocks -= 1
+            # switch black to white
             else:
                 self.board[row][col] = self.emptyColor
                 self.whiteBlocks += 1
+
+    # update method for the grid
+    def update(self):
+        self.connectedComponentCount += [floodFill(self, 1, 1)]
+
     # heuristic functions
 
-
-#     A      B    where A is set of a potential placement of all n polyominos
-#     * ---> *    and B is set of all cells in the grid
-#     * ---> *
-#     * ---> *
-#     * ---> *
-#     * ---> *
-
-# MODEL - INITIALIZATION
+#     A      B    where A is set of a potential placement of all n polyominos
+#     * ---> *    and B is set of all cells in the grid
+#     * ---> *
+#     * ---> *
+#     * ---> *
+#     * ---> *
+# MODEL - INITIALIZATION
 def appStarted(app):
     app.grid = grid()
-    app.tetrisPieces = [ iPiece, jPiece, lPiece, oPiece, sPiece, tPiece, zPiece ]
     app.tetrisPieceColors = [ "red", "yellow", "magenta", "pink", "cyan", "green", "orange" ]
+    app.genButton = button(math.floor(app.width) - (2/12)*app.width + 10, app.height//3 + 100 - 20\
+                        , math.floor(app.width) - (2/12)*app.width + 90, app.height//3 + 100 + 20, "purple")
 
+    app.placementList = graph_backend.generatePlacements(app.grid.cols, app.grid.rows)
+    app.placementList = graph_backend.naiveGenerate(app.grid)
+    app.timerDelay = 100
 
 # VIEW -- DRAW CELL
 def drawCell(app, canvas, row, col, color):
@@ -136,68 +103,100 @@ def drawCell(app, canvas, row, col, color):
                             fill=color)
 
 
-# VIEW -- DRAW COMPONENT INFO TEXT
+# VIEW --DRAW COMPONENT INFO TEXT
 def drawText(app, canvas):
     # 45 MAGIC NUMBER SORRY
     canvas.create_text(math.floor(app.width) - (2/12)*app.width + 45, app.height//3,
-                       text="Connected components: ", font='Arial 8 bold')
+                                    text="Connected Components", font='Arial 8 bold')
     if app.grid.whiteBlocks % 4 == 0:
         fill = 'green'
     else:
         fill = 'red'
     canvas.create_text(math.floor(app.width) - (2/12)*app.width + 45, app.height//3 + 40,
-                       text=f'{app.grid.whiteBlocks} blocks', font='Arial 8 bold', fill=fill)
+                                    text=f'{app.grid.whiteBlocks} blocks',
+                                    font='Arial 8 bold',
+                                    fill=fill)
+    return
 
 
 # VIEW -- DRAW BOARD
 def drawBoard(app, canvas):
     # 50 magic number for right side margin
-    canvas.create_rectangle(0, 0, app.width, app.height, fill = 'orange')
+    canvas.create_rectangle(0,0, app.width, app.height, fill='orange')
     canvas.create_rectangle(app.grid.margin, app.grid.margin,\
-                            app.grid.margin + app.grid.cols*(app.grid.cellSize + 2),\
-                            app.height - app.grid.margin, fill='black')
+                           app.grid.margin + app.grid.cols*(app.grid.cellSize + 2),\
+                           app.height - app.grid.margin, fill="black")
     # Iterate through each row/col in our model
     for row in range(app.grid.rows):
         for col in range(app.grid.cols):
             drawCell(app, canvas, row, col, color=app.grid.board[row][col])
 
+# VIEW - BUTTON CLASS
+class button:
+    def __init__(self, startX, startY, endX, endY, fill):
+        self.startX = startX
+        self.startY = startY
+        self.endX = endX
+        self.endY = endY
+        self.fill = fill
+    def clicked(self, x, y):
+        if(self.startX <= x and self.endX >= x):
+            if(self.startY <= y and self.endY >= y):
+                return True
+    def draw(self, app, canvas):
+        canvas.create_rectangle(self.startX, self.startY, self.endX, self.endY, fill=self.fill)
+        canvas.create_text(math.floor(app.width) - (2/12)*app.width+50, app.height//3 + 100, \
+            text='Generate', font='Arial 8 bold')
+
 
 # CONTROLLER - KEY PRESSES
 def keyPressed(app, event):
-    if event.key == 'Up':
-        pass
+    if event.key == "Up":
+        debug(app)
 
 
 def mousePressed(app, event):
     size = app.grid.cellSize
     margin = app.grid.margin
     col = math.floor((event.x - margin - 2)/(size + 2))
-    row = math.floor((event.y - margin - 2)/(size + 2))
-
+    row =math.floor((event.y - margin - 2)/(size + 2))
     if (row <= app.grid.rows and col <= app.grid.cols and row >= 0 and col >= 0):
-        app.grid.blockifyCell(row, col)
+        app.grid.blockifyCell(row,col)
+    if(app.genButton.clicked(event.x, event.y)):
+        print("BRUH A7A")
 
 
 # VIEW - MAIN DRAW FUNCTION
 def redrawAll(app, canvas):
     drawBoard(app, canvas)
     drawText(app, canvas)
-    return
+    app.genButton.draw(app, canvas)
 
 
-# GAME LOOP
+import time
+from random import*
+## DEBUG FUNCTION
+def debug(app):
+    for placement in app.placementList:
+        randinteger = randint(0, len(app.tetrisPieceColors) - 1)
+        for i in placement:
+            app.grid.board[i//app.grid.cols][i%app.grid.cols] = app.tetrisPieceColors[randinteger]
+
+
+# Game Loop
 def runPolyominoTiling():
+    #setup dimensions
     myDimensions = gridDimensions()
     rows = myDimensions[0]
     cols = myDimensions[1]
     cellSize = myDimensions[2]
     margin = myDimensions[3]
     runApp(width=(round((margin*2 + cols*(cellSize + 2))*1.2)),\
-    height=(margin*2 + rows*(cellSize + 2)))
+        height=(margin*2 + rows*(cellSize + 2)))
 
 
-#################################################
+###############################################################
 # main
-#################################################
-
-runPolyominoTiling()
+###############################################################
+if __name__ == "__main__":
+    runPolyominoTiling()
--- graph_backend.py
+++ graph_backend.py
@@ -1,58 +1,196 @@
+iPiece = [
+    [True, True, True, True]
+]
+
+iPiece90 = [
+    [True],
+    [True],
+    [True],
+    [True]
+]
+
+jPiece = [
+    [ True,  False, False],
+    [ True,   True, True ]
+]
+
+jPiece90 = [
+    [ True, True ],
+    [ True, False],
+    [ True, False]
+]
+
+jPiece180 = [
+    [ True, True, True],
+    [False, False, True]
+]
+
+jPiece270 = [
+    [ False, True ],
+    [ False, True ],
+    [  True, True ]
+]
+
+lPiece = [
+    [ False, False,  True],
+    [ True, True,  True]
+]
+
+lPiece90 = [
+    [True, True],
+    [False, True],
+    [False, True]
+]
+
+lPiece180 =[
+    [False, False, True],
+    [True, True, True]
+]
+
+lPiece270 = [
+    [True, False],
+    [True, False],
+    [True, True]
+]
+
+oPiece = [
+    [True, True],
+    [True, True]
+]
+
+sPiece = [
+    [False, True, True],
+    [True, True, False]
+
+]
+
+sPiece90 = [
+    [True, False],
+    [True, True],
+    [False, True]
+]
+
+tPiece = [
+    [False, True, False],
+    [True, True, True]
+]
+
+tPiece90 = [
+    [False, True],
+    [True, True],
+    [False, True]
+]
+
+
+tPiece180 = [
+    [True, True, True],
+    [False, True, False]
+
+]
+
+tPiece270 = [
+    [True, False],
+    [True, True],
+    [True, False]
+
+]
+
+zPiece = [
+    [True, True, False],
+    [False, True, True]
+]
+
+zPiece90 = [
+    [False, True],
+    [True, True ],
+    [True, False]
+]
+
+allPieces = [iPiece,iPiece90,jPiece,jPiece90, jPiece180,\
+    jPiece270, lPiece, lPiece90, lPiece180, lPiece270,\
+    oPiece, sPiece, sPiece90, tPiece,tPiece90,\
+    tPiece180, tPiece270, zPiece, zPiece90]
+
+## File for handling graph backend
+## We are representing potential polyomino tilings as a bipartite graph
+## where nodes on the left side are the set of polyominos tiled and right
+## side is the cells on the grid and the edges in between signify which
+## polyomino occupies how many and which cells. Our problem will be trying
+## to find an exact cover, aka a subset of A whose union of neighborhoods
+## pairwise disjointly covers all cells in B
+# BASIC CONSTS
+# Seven "standard" pieces (tetrominoes)
+# Initialize the pieces
+
+# Takes an upperbound n to form a big enough n x n board that
+# encloses current board
+def generatePlacements(width, height):
+    genericPieces = []
+    n = width*height
+    for i in range(n):
+        # Check if generic piece placement holds
+        if i % width <= width - 4:  # Horizontal iPiece 0 deg
+            genericPieces.append({i, i+1, i+2, i+3})
+        if i <= n - 3*width - 1:  # Vertical iPiece 90 deg
+            genericPieces.append({i, width+i, 2*width+i, 3*width+i})
+        if i % width <= width - 3 and i < n - width:  # jPiece 0 deg
+            genericPieces.append({i, width+i, width+i+1, width+i+2})
+        if i % width != width-1 and i < n - 2*width:  # jPiece 90 deg
+            genericPieces.append({i, i+1, width+i, 2*width+i})
+        if i % width <= width - 3 and i < n - width:  # jPiece 180 deg
+            genericPieces.append({i, i+1, i+2, width+i+2})
+        if i % width != width - 1 and i < n - 2*width:  # jPiece 270 deg
+            genericPieces.append({i+1, width+i+1, 2*width+i, 2*width+i+1})
+        if i % width <= width - 3 and i < n - width:  # lPiece 0 deg
+            genericPieces.append({i+2, width+i, width+i+1, width+i+2})
+        if i % width != width - 1 and i < n - 2*width:  # lPiece 90 deg
+            genericPieces.append({i, i+1, width+i+1, 2*width+i+1})
+        if i % width <= width - 3 and i < n - width:  # lPiece 180
+            genericPieces.append({i, i+1, i+2, width+i})
+        if i % width != width-1 and i < n - 2*width:  # lPiece 270
+            genericPieces.append({i, width+i, 2*width+i, 2*width+i+1})
+        if i % width != width - 1 and i < n - width:  # oPiece
+            genericPieces.append({i, i + 1, width + i, width + i + 1})
+        if i % width <= width - 3 and i < n - width:  # sPiece 0 deg
+            genericPieces.append({i + 1, i + 2, width + i, width + i + 1})
+        if i % width != width - 1 and i < n - 2 * width:  # sPiece 90 deg
+            genericPieces.append({i, width + i, width + i + 1, 2 * width + i + 1})
+        if i % width <= width - 3 and i < n - width:  # tPiece 0 deg
+            genericPieces.append({i + 1, width + i, width + i + 1, width + i + 2})
+        if i % width != width - 1 and i < n - 2 * width:  # tPiece 90 deg
+            genericPieces.append({i + 1, width + i, width + i + 1, 2 * width + i + 1})
+        if i % width <= width - 3 and i < n - width:  # tPiece 180 deg
+            genericPieces.append({i, i + 1, i + 2, width + i + 1})
+        if i % width != width - 1 and i < n - 2 * width:  # tPiece 270 deg
+            genericPieces.append({i, width + i, width + i + 1, 2 * width + i})
+        if i % width <= width - 3 and i < n - width:  # zPiece 0 deg
+            genericPieces.append({i, i + 1, width + i + 2, width + i + 1})
+        if i % width != width - 1 and i < n - 2 * width:  # zPiece 90 deg
+            genericPieces.append({i + 1, width + i, width + i + 1, 2 * width + i})
+    return genericPieces
+
+
+
+
+
+####### GENERATING LEFT BIPARTITION ########
+#  Functions to
+## Generate all possible placements of all polyominos
+
 #
-## File for handling graph backend
-## We are representing potential polyomino tilings as a bipartite graph
-## where nodes on the left side are the set of polyominos tiled and right
-## side is the cells on the grid and the edges in between signify which
-## polyomino occupies how many and which cells. Our problem will be trying
-## to find an exact cover, aka a subset of A whose union of neighborhoods
-## pairwise disjointly covers all cells in B
-
-LEFT = 0
-RIGHT = 1
-### 0 for left 1 for right
-### Boilerplate adapted from geeksforgeeks
-# A class to represent the adjacency list of the node
-class AdjNode:
-    def __init__(self, data, bipartition):
-        self.vertex = data
-        self.next = None
-        self.bipartition = bipartition
-
-
-# A class to represent a graph. A graph
-# is the list of the adjacency lists.
-# Size of the array will be the no. of the
-# vertices "V"
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [None] * self.V
-
-    # Function to add an edge in an undirected graph
-    def add_edge(self, src, dest):
-        # Adding the node to the source node
-        node = AdjNode(dest)
-        node.next = self.graph[src]
-        self.graph[src] = node
-
-        # Adding the source node to the destination as
-        # it is the undirected graph
-        node = AdjNode(src)
-        node.next = self.graph[dest]
-        self.graph[dest] = node
-
-
-# Graph representation data structure for representing graph tiling
-## Header for bipartite graph representation of the grid
-class bipartite:
-    # A and B are the sets
-    def __init__(self, grid):
-        self.grid = grid
-        self.graph = self.initializeGraph()
-
-    def initializeGraph(self):
-        pass
-
-    # bool return can a subset have a perfect matching with B or not
-    def checkExactCover(self):
-        pass
+## Naive specification function
+def naiveGenerate(app):
+    placedPieces = []
+    for piece in allPieces:
+        for row in range(app.rows):
+            for col in range(app.cols):
+                placementWorks = True
+                placementIndices = []
+                for pieceRow in range(len(piece)):
+                    for pieceCol in range(len(piece[0])):
+                        if row+pieceRow >= 1 and row+pieceRow < app.rows-1 and col+pieceCol >=1 and col+pieceCol < app.cols-1:
+                            placementWorks = False
+                        placementIndices.append(row+pieceRow*app.cols + col+pieceCol)
+                if placementWorks:
+                    placedPieces.append(placementIndices)
+    return placedPieces
